402dcc241b1c4b7b4ed82e0920ed9e8630c76f75
aromolagos <108009155+aromolagos@users.noreply.github.com>
2022-06-22 10:39:42 -0400

    Update app.py
    carga de datos csv

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("Listado de comida")
 
@@ -7,3 +8,10 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+#Agregar una seccion para crear tu batido
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#Cargar un listado de datos en formato csv para mostrarlo en la applicacion
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
