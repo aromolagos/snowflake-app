c922b1c127bee3f2702a34bfd3afc4177ad1cfd5
aromolagos <108009155+aromolagos@users.noreply.github.com>
2022-06-22 10:47:55 -0400

    Update app.py
    - Creacion de seleccion multiple y filtrado de tabla nutricional

--- app.py
+++ app.py
@@ -14,4 +14,16 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Cargar un listado de datos en formato csv para mostrarlo en la applicacion
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
+
+#crear un filtrado con multiples selecciones
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#Seleccion multiple
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+#Filtro a nivel de tabla
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#Tabla filtrada frutas con informacion nutricional
+streamlit.dataframe(fruits_to_show)
