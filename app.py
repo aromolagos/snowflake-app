c07b8a905c7a51135ee732098d3ffbf73134aee8
aromolagos <108009155+aromolagos@users.noreply.github.com>
2022-06-22 10:34:08 -0400

    Update app.py
    lista de menu

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('Hola esto es un titulo')
+streamlit.title("Listado de comida")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
